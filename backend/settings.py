34684d40412b6646743a38b55895a74890af5b1c
Mark Nguy <56264886+Qischer@users.noreply.github.com>
2025-01-27 21:57:27 -0500

    first view

--- backend/settings.py
+++ backend/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        'NAME': os.environ.get('POSTGRES_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
-        'PORT': 5432,
+        "ENGINE": os.environ.get('POSTGRES_ENGINE', "django.db.backends.sqlite3"), 
+        'NAME': os.environ.get('POSTGRES_NAME', BASE_DIR / "db.sqlite3"),
+        'USER': os.environ.get('POSTGRES_USER', "user"),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', "password"),
+        'HOST': os.environ.get('POSTGRES_HOST', "localhost"),
+        'PORT': os.environ.get('POSTGRES_PORT', "5432"),
     }
 }
 
--- websrv/apps.py
+++ websrv/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppConfig(AppConfig):
+class WebsrvConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "app"
+    name = "websrv"
