d08af01d8480199abfe2c93320970b53a2aa0e2c
Hank-Chiang30 <hankchiang30@gmail.com>
2025-02-19 03:24:18 -0500

    Create urls & Bills_Data
    Create URLS for recommended and trending. Also create views with bills_data

--- websrv/urls.py
+++ websrv/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path("home", views.index, name="index"),
+    path("home/trending_bills", views.trending_bills, name="trending_bills"),
+    path("home/recommended_bills", views.recommended_bills, name="recommended_bills"),
+
 ]
