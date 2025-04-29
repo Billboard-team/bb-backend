88ce32f3d60873844dfeae430dd4d24b91c73982
Hank-Chiang30 <hankchiang30@gmail.com>
2025-04-29 19:24:55 -0400

    Revert "updated"
    This reverts commit 56ceb0c2e74597e83e828e3d004eabdfb66fd8ca.

--- websrv/urls.py
+++ websrv/urls.py
@@ -1,39 +1,22 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 from . import views
 from . import auth_views
-from .comment_views import CommentViewSet
-
-router = DefaultRouter()
-router.register(r'comments', CommentViewSet, basename='comment')
 
 urlpatterns = [
     path("", views.index, name="index"),
-
-
     path("bills/trending", views.trending_bills, name="trending_bills"),
+    path("bills/trending/education", views.trending_bills_education, name="trending_bills_education"),
     path("bills/recommended", views.recommended_bills, name="recommended_bills"),
 
-    path("members/<str:bioguide_id>", views.get_member_data, name="member_detailed_view"),  
-
-    path("bills/<int:id>", views.get_bill_detailed, name="bill_detailed_view"),  
+    path("bills/<int:id>", views.single_bill, name="single_bill"),  
 
     path("bills/<int:id>/text", views.get_bill_text_original, name="bill_text_original"),  
     path("bills/<int:id>/text/summarized", views.get_bill_text_summarized, name="bill_text_summarized"),  
     path("bills/<int:id>/text/sources", views.get_bill_text_sources, name="bill_text_sources"),  
-
     path("me/", auth_views.me_view),
     path("me/update/", auth_views.update_profile_view),
     path("auth0-logs/", auth_views.auth0_log_webhook),
-    path("me/delete/", auth_views.delete_account_view),
-    path('me/activity-stats/', views.get_user_activity_stats, name='get_user_activity_stats'),
-
-    path('bills/<int:id>/view/', views.record_bill_view, name='record_bill_view'),
-    path('bills/view-history/', views.get_bill_view_history, name='get_bill_view_history'),
-
     path("tags/", auth_views.list_expertise_tags),
     path("profile/tags/", auth_views.update_expertise_tags),
-    path("update/", auth_views.update_profile_view),
-    # Include DRF router URLs (includes all comment endpoints)
-    path('', include(router.urls)),
+    path("update/", auth_views.update_profile_view)
 ]
